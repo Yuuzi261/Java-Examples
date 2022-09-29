7245a998396d1ecd3cc398875f6882f96f44bba3
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2022-09-29 23:26:21 +0800

    feat: add 4.19

---
+++ ch4&ch5/4.19/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+        int[] sales = new int[4];
+        double commission = 0;
+		double[] items = {239.99, 129.75, 99.95, 350.89};
+
+        Scanner input = new Scanner(System.in);
+
+        for(int i = 0;i < 4;i++) {
+            System.out.printf("Enter the sales amounts of item %d: ", i + 1);
+            sales[i] = input.nextInt();
+        }
+
+        for(int i = 0;i < 4;i++) {
+            commission += sales[i] * items[i];
+        }
+
+        commission = commission * 0.09 + 200;
+        System.out.printf("Commission: $%f", commission);
+
+        input.close();
+
+	}
+}
